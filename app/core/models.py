f4316c72ac258e44476fd811f88c2f9ecd2daa2b
elasthiel <elasthiel@gmail.com>
2022-12-15 17:27:01 +0800

    Add custom User Model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,39 @@
-from django.db import models  # noqa
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManger(BaseUserManager):
+    """
+    Manager for users
+    """
+    def create_user(self, email, password=None, **kwargs):
+        """
+        Create, save, and return a new user
+        **kwargs - For extra fields
+        """
+        user = self.model(email=email, **kwargs)
+
+        # password is set here for hashing
+        user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    User in the system
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManger()
+
+    # Defined field for user authentication
+    USERNAME_FIELD = 'email'
