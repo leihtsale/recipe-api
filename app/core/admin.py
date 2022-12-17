ba03bd90e29f8796768acc4021de01668d6da7a5
elasthiel <elasthiel@gmail.com>
2022-12-17 09:39:34 +0800

    Add the proper fieldsets to fix the issue of not loading the edit page for user

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,6 +4,13 @@ Django admin customization
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+"""
+Added to handle if the language is changed, and if you want to change it everywhere
+in the project.
+"""
+from django.utils.translation import gettext_lazy as _
+
 from core import models
 
 class UserAdmin(BaseUserAdmin):
@@ -12,6 +19,20 @@ class UserAdmin(BaseUserAdmin):
     """
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'), {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
 
+    readonly_fields = ['last_login']
 
 admin.site.register(models.User, UserAdmin)
