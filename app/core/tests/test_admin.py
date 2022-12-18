1f29beed293818b85669521c9e4dfdea83368f15
elasthiel <elasthiel@gmail.com>
2022-12-18 16:20:21 +0800

    Add Test for the "create user" page in Admin page and implement it by adding the required fieldsets in admin.py

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -48,3 +48,12 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """
+        Test for the 'create user' page, test if it works
+        """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
