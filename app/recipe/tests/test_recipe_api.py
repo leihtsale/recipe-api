90a0c36b12919c68d7628c61949cbf6be3c4678c
elasthiel <elasthiel@gmail.com>
2023-01-07 04:49:59 +0800

    Add test for getting the recipe detail

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -5,10 +5,17 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Recipe
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 
 RECIPES_URL = reverse('recipe:recipe-list')
+
+
+def detail_url(recipe_id):
+    """
+    Helper function to return the recipe detail url
+    """
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 
 def create_recipe(user, **params):
@@ -95,6 +102,20 @@ class PrivateRecipeApiTests(TestCase):
         res = self.client.get(RECIPES_URL)
         recipes = Recipe.objects.filter(user=self.user)
         serializer = RecipeSerializer(recipes, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """
+        Check if the detail for a specific recipe is correct
+        should return 200-OK and the recipe details
+        """
+        recipe = create_recipe(user=self.user)
+        recipe_url = detail_url(recipe.id)
+
+        res = self.client.get(recipe_url)
+        serializer = RecipeDetailSerializer(recipe)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
